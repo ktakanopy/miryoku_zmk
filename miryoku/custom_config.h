1f0306da5010ae83dc9df4a9cd7bb60e6de345b5
kevin takano <kevin.takano@gmail.com>
2024-10-19 13:56:06 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,23 +2,16 @@
 
 #define MIRYOKU_CLIPBOARD_MAC
 
-// Copyright 2022 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-#if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
-
-#define XXX &none
-
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+TAB  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  F20 \
+LSHFT K10  K11  K12  K13  K14      K15  K16  K17  K18  K19  RSHFT \
+RCTRL K20  K21  K22  K23  K24      K25  K26  K27  K28  K29  RCTRL \
+               LCMD  K33  ESC       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -35,5 +28,3 @@ XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
 
 
 #endif
-
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_CORNE
