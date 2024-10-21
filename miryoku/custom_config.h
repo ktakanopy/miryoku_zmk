c15bebe66f136a626bf4c4c2d0bbe2490186e583
kevin takano <kevin.takano@gmail.com>
2024-10-21 15:19:53 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,7 +11,7 @@
 &kp TAB  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp RCTRL \
 &kp LALT K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp LALT \
 &kp LSHFT K20  K21  K22  K23  K24      K25  K26  K27  K28  K29  &kp RSHFT \
-                    ,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL)      K35  K36  K37
+                    U_LT(U_SYM, RET)  U_LT(U_NUM, BSPC) U_LT(U_FUN, DEL)      K35  K36  K37
 
 // #define MIRYOKU_LAYER_BASE \
 // &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
