02ad2b217027bb2750e5b265673bb520fb911472
kevin takano <kevin.takano@gmail.com>
2024-10-21 15:40:20 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -19,11 +19,17 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, LGUI),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, ESC),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
+#define CUSTOM_BEHAVIORS \
+    &macro_tilde_space { \
+        compatible = "zmk,behavior-macro"; \
+        bindings = <&kp LS(GRAVE)>, <&kp SPACE>; \
+    };
+
 
 #define MIRYOKU_LAYER_SYM \
-(&kp TILDE &kp SPACE),          &kp AMPS,          &kp ASTRK,         &kp LBRC,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+MACRO_TILDE_SPACE,          &kp AMPS,          &kp ASTRK,         &kp LBRC,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
+&kp TILDE2,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 
