8d2a85eaf0f900373fc94b448a92cf3a006a71a8
kevin takano <kevin.takano@gmail.com>
2024-10-21 15:48:45 -0400

    Update custom_config.h macro

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -20,10 +20,12 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp 
 U_NP,              U_NP,              U_LT(U_MEDIA, LGUI),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, ESC),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define CUSTOM_BEHAVIORS \
+macros {\
     &macro_tilde_space { \
         compatible = "zmk,behavior-macro"; \
         bindings = <&kp LS(GRAVE)>, <&kp SPACE>; \
-    };
+    }; \
+}
 
 
 #define MIRYOKU_LAYER_SYM \
