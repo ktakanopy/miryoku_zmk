dde8d382e04bd3da3cb618e58dbd6bdeac05c0ab
kevin takano <kevin.takano@gmail.com>
2024-10-24 11:10:15 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,7 +32,7 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X),   U_LT(U_MEDIA, C),   &kp V,              &kp 
 U_NP,             U_NP,             U_LT(U_MOUSE, TAB), U_LT(U_NAV, SPACE), U_LT(U_BUTTON, ESC), U_LT(U_SYM, RET),   U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),   U_NP,               U_NP
 
 #define MIRYOKU_LAYER_SYM \
-&kp EXCL,          &kp AT,           &kp HASH,           &kp DLLR,           &kp PRCNT,          U_NA,              &u_to_U_BASE,       &u_to_U_EXTRA,      &u_to_U_TAP,       U_BOOT,        \
+&kp GRAVE,         &kp EXCL,          &kp AT,           &kp HASH,           &kp DLLR,           &kp PRCNT,           &u_to_U_BASE,       &u_to_U_EXTRA,      &u_to_U_TAP,       U_BOOT,        \
 &kp LBKT,          &kp LPAR,         &kp RPAR,           &kp RBKT,           &kp CARET,          &kp BACKSLASH,      &kp LSHFT,          &kp LGUI,           &kp LALT,          &kp LCTRL,           \
 &kp AMPS,          &kp LBRC,         &kp RBRC,           &g_under,           &kp PIPE,           &kp BACKSLASH,      &u_to_U_SYM,        &u_to_U_MOUSE,      &kp RALT,          U_NA,               \
 U_NP,              U_NP,             &kp QMARK,          &kp COLON,          &kp UNDER,          U_NA,               U_NA,               U_NA,               U_NP,              U_NP
@@ -44,17 +44,18 @@ U_NP,              U_NP,             &kp QMARK,          &kp COLON,          &kp
 U_NP,              U_NP,             &kp COMMA,          &kp DOT,            &kp EQUAL,          U_NA,               U_NA,               U_NA,               U_NP,               U_NP
 
 #define MIRYOKU_LAYER_NAV \
-&kp GRAVE,         &kp LG(LBRC),     &kp LG(RBRC),       &kp LG(LBKT),     &kp LG(RBKT),        U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LG(GRAVE),         &kp LG(LBRC),     &kp LG(RBRC),       &kp LG(LBKT),     &kp LG(RBKT),        U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LCTRL,         &kp LALT,         &kp LGUI,            &kp LSHFT,         U_NA,                &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 U_NA,              &kp ENTER,        &kp BSPC,       U_NA,       U_NA,               &kp HOME,           &kp PG_DN,          &kp PG_UP,          &kp END,           &kp INS,           \
 U_NP,              U_NP,             U_NA,               U_NA,               U_NA,               &kp RET,            &kp BSPC,           &kp DEL,            U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_MOUSE \
-&kp LG(GRAVE),         &kp LG(LBRC),     &kp LG(RBRC),       &kp LG(LBKT),     &kp LG(RBKT),        U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LCTRL,         &kp LALT,         &kp LGUI,            &kp LSHFT,         &kp LG(TAB),                &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
-U_NA,              &kp ENTER,        &kp BSPC,       U_NA,       U_NA,               &kp HOME,           &kp PG_DN,          &kp PG_UP,          &kp END,           &kp INS,           \
-U_NP,              U_NP,             U_NA,               U_NA,               U_NA,               &kp RET,            &kp BSPC,           &kp DEL,            U_NP,              U_NP
+U_BOOT,         &u_to_U_TAP,      &u_to_U_EXTRA,      &u_to_U_BASE,       U_NA,               U_RDO,              U_PST,              U_CPY,              U_CUT,             U_UND,             \
+&kp LCTRL,         &kp LALT,         &kp LGUI,           &kp LSHFT,          U_NA,               U_MS_L,             U_MS_D,             U_MS_U,             U_MS_R,            U_NU,              \
+U_NA,              U_NA,             &tilde_macro,       &g_under,           &kp LC(A),          U_WH_L,             U_WH_D,             U_WH_U,             U_WH_R,            U_NU,              \
+U_NP,              U_NP,             U_NA,               U_NA,               U_NA,               U_BTN2,             U_BTN1,             U_BTN3,             U_NP,              U_NP
+
 // Combo definitions
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
