134823e1762aa24a8487018bdce529ba4a81be0e
kevin takano <kevin.takano@gmail.com>
2024-10-24 09:25:10 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -45,8 +45,8 @@ U_NP,              U_NP,             &kp COMMA,          &kp DOT,            &kp
 
 #define MIRYOKU_LAYER_NAV \
 &kp GRAVE,         &kp LG(LBRC),     &kp LG(RBRC),       &kp LG(LBKT),     &kp LG(RBKT),        U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LCTRL,         &kp LALT,        &kp LGUI,            &kp LSHFT,         U_NA,                &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
-U_NA,              &kp RALT,         &kp LG(LBRC),       &kp LG(RBRC),       U_NA,               &kp HOME,           &kp PG_DN,          &kp PG_UP,          &kp END,           &kp INS,           \
+&kp LCTRL,         &kp LALT,         &kp LGUI,            &kp LSHFT,         U_NA,                &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
+U_NA,              &kp ENTER,        &kp BSPC,       U_NA,       U_NA,               &kp HOME,           &kp PG_DN,          &kp PG_UP,          &kp END,           &kp INS,           \
 U_NP,              U_NP,             U_NA,               U_NA,               U_NA,               &kp RET,            &kp BSPC,           &kp DEL,            U_NP,              U_NP
 
 
