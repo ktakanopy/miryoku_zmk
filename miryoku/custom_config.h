17588852c97c5c0dc348eb3b88f3db5be7f89f5f
kevin takano <kevin.takano@gmail.com>
2024-10-27 16:34:29 -0400

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -33,8 +33,8 @@ U_NP,             U_NP,             U_LT(U_BUTTON, ESC), U_LT(U_NAV, SPACE), U_L
 
 #define MIRYOKU_LAYER_SYM \
 &u_to_U_BASE,      &u_to_U_EXTRA,    &u_to_U_TAP,        &kp SEMI,             &kp GRAVE,         &kp EXCL,            &kp AT,           &kp HASH,            &kp DLLR,           &kp PRCNT,       \
-&kp LCTRL,         &kp LALT,         &kp LGUI,           &kp LSHFT,          &tilde_macro ,      &kp LBKT,           &kp CARET,         &kp LPAR,           &kp RPAR,           &kp RBKT,                   \
-&u_to_U_SYM,       &u_to_U_MOUSE,    &kp RALT,           &kp SLASH,               &kp TILDE,          &kp AMPS,            &kp PIPE,           &kp LBRC,           &kp RBRC,           &g_under,               \
+&kp LCTRL,         &kp LALT,         &kp LGUI,           &kp LSHFT,          &kp TILDE,      &kp LBKT,           &kp CARET,         &kp LPAR,           &kp RPAR,           &kp RBKT,                   \
+&u_to_U_SYM,       &u_to_U_MOUSE,    &kp RALT,           &kp SLASH,           &tilde_macro,          &kp AMPS,            &kp PIPE,           &kp LBRC,           &kp RBRC,           &g_under,               \
 U_NP,              U_NP,             &kp UNDER,          &kp COLON,          &kp QMARK,          U_NA,               U_NA,               U_NA,               U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
